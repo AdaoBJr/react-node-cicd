6758a7703ba6a75c3c70cb069dcfec5efb708203
AdaoBJr <adaobjr@gmail.com>
2022-09-07 22:03:32 -0400

    feat: :art: added express.static in startServer on api

--- api/src/startServer.ts
+++ api/src/startServer.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express, { Response } from 'express';
+import path from 'path';
 
 const PORT = 3001;
 
 const startServer = () => {
   const app = express();
+  app.use(express.static(path.join(__dirname, '..', 'app', 'build')));
   app.use(express.json());
 
   app.get('/example', (_, response: Response) => {
